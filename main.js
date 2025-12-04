4bdb002db88a268717574985ee300c20d0c7d24c
arggbl <spam92606@gmail.com>
2025-12-03 19:36:11 -0800

    Update main.js

--- main.js
+++ main.js
@@ -1,5 +1,6 @@
 function squish() {
     let img = document.getElementById('zerb')
+    img.disabled = true;
     img.width = 480
     img.height = 400
     setTimeout(function() { unSquish(); }, 1000)
@@ -8,4 +9,5 @@ function unSquish() {
     let img = document.getElementById('zerb')
     img.width = 480
     img.height = 480
+    img.disabled = false;
 }
