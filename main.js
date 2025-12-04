434e1cbe8e96ab01a938b92987bcf0138908e6e2
arggbl <spam92606@gmail.com>
2025-12-03 19:15:20 -0800

    Update main.js

--- main.js
+++ main.js
@@ -1,1 +1,16 @@
 
+function squish() {
+    let img = document.getElementById('zerb')
+    img.width = 480
+    img.height = 400
+    setTimeout(function() { unSquish(); }, 1000)
+}
+function unSquish() {
+    let img = document.getElementById('zerb')
+    img.width = 480
+    img.height = 480
+}
+
+function MinClicked() {
+    squish()
+}
